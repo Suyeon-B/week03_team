5a8fbbb8ddf72872d50d3aa9e0e7ef45ec57d483
Suyeon-B <tndus4243@naver.com>
2022-04-16 00:52:52 +0900

    11724 시간복잡도 개선

--- suyeon/그래프 탐색 기본/11724 연결 요소의 개수.py
+++ suyeon/그래프 탐색 기본/11724 연결 요소의 개수.py
@@ -1,6 +1,6 @@
-# 시간초과 -> visited 다르게 구현해서 시간복잡도 낮추기
+# visited를 미리 배열로 지정해주어 시간 복잡도 개선
 import sys, collections
-from collections import deque
+sys.setrecursionlimit(10**6) # python RecursionError 방지
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
@@ -8,22 +8,24 @@ n, m = map(int, input().split())
 graph = collections.defaultdict(list) # 빈 그래프 생성, default는 list
 for i in range(1, m+1):
     vertex1, vertex2 = map(int, input().split())
-    graph[vertex1] = sorted(graph[vertex1]+[vertex2])
-    graph[vertex2] = sorted(graph[vertex2]+[vertex1]) # 무방향 그래프이므로
+    graph[vertex1] = graph[vertex1]+[vertex2]
+    graph[vertex2] = graph[vertex2]+[vertex1] # 무방향 그래프이므로
 
 # recursive DFS
-def DFS(start_v, visited=[]):
-    visited.append(start_v)
+visited = [False] * (n+1)
+def DFS(start_v):
+    visited[start_v] = True
     for v in graph[start_v]:
-        if v not in visited:
-            visited = DFS(v, visited)
-    return visited
+        if not visited[v]:
+            DFS(v)
 
-visited = DFS(1)
+DFS(1) # 시작 노드는 항상 1이다.
+
+# Connected component 개수 구하기
 cnt = 1
 for i in range(1, n+1):
-    if i not in visited:
-        visited = DFS(i)
+    if not visited[i]:
+        DFS(i) # 아직 탐색하지 않은 노드를 시작노드로 다시 Connected component 찾기
         cnt+=1
 
 print(cnt)
