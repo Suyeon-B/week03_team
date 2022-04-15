c930f13fe55c766843571215eddcd8c7182131f2
dd0114 <ehgus0114@gmail.com>
2022-04-15 23:00:56 +0900

    dh_1~6

---
+++ dd0114/01_1991.py
@@ -0,0 +1,46 @@
+import sys
+
+N = int(input())
+
+class Node :
+    def __init__(self, key,left,right):
+        self.key = key
+        self.left = left
+        self.right = right
+
+tree = {}
+
+for i in range(N):
+    n, l, r = map(str,input().split())
+    # 트리 딕셔너리에 저장한다는 뜻인데 node가 의미하는 것은? 각 노드의 이름?
+    tree[n] = Node(key=n, left=l, right= r)
+
+
+def preorder(node):
+    print(node.key, end='')
+    if node.left != '.':
+        preorder(tree[node.left])
+    if node.right != '.':
+        preorder(tree[node.right])
+
+def inorder(node):
+    if node.left != '.':
+        inorder(tree[node.left])
+    print(node.key,end='')
+    if node.right != '.':
+        inorder(tree[node.right])
+
+def postorder(node) :
+    if node.left != '.':
+        postorder(tree[node.left])
+    if node.right != '.':
+        postorder(tree[node.right])
+    print(node.key, end='')
+
+print(preorder(tree['A']))
+print()
+print(inorder(tree['A']))
+print()
+print(postorder(tree['A']))
+
+# 질문!! none 나오는 이유는??
---
+++ dd0114/test.py
@@ -0,0 +1,3 @@
+cost, a, b = [7,3,5]
+
+print(cost)
